75986861ea606ef9afcfc85c122daa2e22045485
jenerationx <jenerationx74@gmail.com>
2019-06-08 21:22:32 -0400

    seed db

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -123,14 +123,14 @@ const bookSeed = [
   }
 ];
 
-db.Book
-  .remove({})
-  .then(() => db.Book.collection.insertMany(bookSeed))
-  .then(data => {
-    console.log(data.result.n + " records inserted!");
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+// db.Book
+//   .remove({})
+//   .then(() => db.Book.collection.insertMany(bookSeed))
+//   .then(data => {
+//     console.log(data.result.n + " records inserted!");
+//     process.exit(0);
+//   })
+//   .catch(err => {
+//     console.error(err);
+//     process.exit(1);
+//   });

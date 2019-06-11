e573235e15fe8650eda0b6d4eeaac1594622409f
jenerationx <jenerationx74@gmail.com>
2019-06-11 13:45:22 -0400

    add mlab

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
 import React from "react";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SaveBooks from "./pages/SaveBooks";
+import SearchBooks from "./pages/SearchBooks";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 function App() {
   return (
-    <div>
-      <Router>
-        <div>
-          <Nav />
-          <Switch>
-            <Route exact path="/" component={Books} />
-            <Route exact path="/books" component={Books} />
-            <Route component={NoMatch} />
-            <Route path="/detail" component={Detail} />
-          </Switch>
-          </div>
-      </Router>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={SearchBooks} />
+          <Route exact path="/saved" component={SaveBooks} />
+          <Route exact path="/saved/:id" component={SaveBooks} />
+          <Route component={NoMatch} /> 
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,12 +1,12 @@
-// const mongoose = require("mongoose");
-// const db = require("../models");
+const mongoose = require("mongoose");
+const db = require("../models");
 
-// // This file empties the Books collection and inserts the books below
+// This file empties the Books collection and inserts the books below
 
-// mongoose.connect(
-//   process.env.MONGODB_URI ||
-//   "mongodb://localhost/reactreadinglist"
-// );
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/reactreadinglist"
+);
 
 const bookSeed = [
   {
@@ -123,14 +123,14 @@ const bookSeed = [
   }
 ];
 
-// db.Book
-//   .remove({})
-//   .then(() => db.Book.collection.insertMany(bookSeed))
-//   .then(data => {
-//     console.log(data.result.n + " records inserted!");
-//     process.exit(0);
-//   })
-//   .catch(err => {
-//     console.error(err);
-//     process.exit(1);
-//   });
+db.Book
+  .remove({})
+  .then(() => db.Book.collection.insertMany(bookSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
